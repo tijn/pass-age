335dbd9665b2304d61289d73795bcacd2a1ffa63
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2018-07-21 15:48:35 +0200

    Create the commands from my old pass-patch
    See the discussion in the newsgroup (password-store@lists.zx2c4.com)
    around 2015-07-26. (Almost 3 years ago!)

---
+++ age.bash
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# - assume the password is stored in the first line of a password-file
+# - find the latest git revision that changes that line
+# - show all passwords by age
+
+
+PREFIX="${PASSWORD_STORE_DIR:-$HOME/.password-store}"
+export GIT_DIR="${PASSWORD_STORE_GIT:-$PREFIX}/.git"
+
+git_revisions() {
+ [[ -d $GIT_DIR ]] || return
+ local path="$1"
+ local passfile="$path.gpg"
+ git log --format=%H -- $passfile
+}
+
+git_revision() {
+ [[ -d $GIT_DIR ]] || return
+ local path="$1"
+ local revision="$2"
+ local passfile="$path.gpg"
+ git show $revision:$passfile | $GPG -d "${GPG_OPTS[@]}" | head -n 1
+}
+
+oldest_password_change() {
+ [[ -d $GIT_DIR ]] || die "Error: the password store is not a git repository. Try \"$PROGRAMgit init\"."
+ local path="$1"
+ check_sneaky_paths "$path"
+ git_revisions "$path" | while read revision
+ do
+   if [ -z "$password" ]; then
+     password="$(git_revision $path $revision)"
+   else
+     if [ password != "$(git_revision $path $revision)" ]; then
+       break
+     fi
+   fi
+   echo $revision
+ done | tail -n 1
+}
+
+show_usage() {
+  echo "Usage:"
+  echo "    pass age PASS-NAME"
+}
+
+cmd_age() {
+  local path="$1"
+  check_sneaky_paths "$path"
+  local oldest=$(oldest_password_change "$path")
+  git show -s --format="%ct%x09%cr%x09"$path"" "$oldest"
+}
+
+case $1 in
+  --help) show_usage ;;
+  "") show_usage ;;
+  *) cmd_age "$@" ;;
+esac
---
+++ ages.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$1" == "--help" ]
+then
+  echo "Usage:"
+  echo "    pass ages"
+  echo "        Show ages of all passwords in the store"
+  echo "    pass ages SUBDIR"
+  echo "        Show ages of all passwords in a certain subdirectory in the store"
+  exit 0
+fi
+
+PREFIX="${PASSWORD_STORE_DIR:-$HOME/.password-store}"
+SUBDIR="${1:-.}"
+
+cd $PREFIX && find $SUBDIR -name "*.gpg" | sed 's/^\.\///' | sed 's/\.gpg$//' | while read path
+do
+  pass age "$path"
+done
